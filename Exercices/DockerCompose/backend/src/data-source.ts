e17919b04adff5e92528ccec1eae2e6e1269695b
Philippe Stons <philippe.stons@bstorm.be>
2026-03-27 09:44:57 +0100

    [FIX]

--- Exercices/DockerCompose/backend/src/data-source.ts
+++ Exercices/DockerCompose/backend/src/data-source.ts
@@ -13,7 +13,7 @@ export const AppDataSource = new DataSource({
     name: "default",
     type: "postgres",
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    port: parseInt(process.env.DB_PORT, 10) || 5432,
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
